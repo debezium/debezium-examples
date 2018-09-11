1092632c4cdc2e0eaf2d1bfc7da60bb1d08fd243
Yuvaraj Loganathan <uvaraj6@gmail.com>
2018-09-11 20:01:39 +0530

    Implemented the review contents.

--- apache-pulsar/src/main/java/io/debezium/examples/apache/pulsar/config/PropertyLoader.java
+++ apache-pulsar/src/main/java/io/debezium/examples/apache/pulsar/config/PropertyLoader.java
@@ -1,32 +1,21 @@
 /*
+ * Copyright Debezium Authors.
  *
- * Copyright (c) 2018 Nutanix, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * @author Yuvaraj L
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
  */
 package io.debezium.examples.apache.pulsar.config;
+
+import org.apache.log4j.Logger;
+
 import java.util.Enumeration;
 import java.util.Properties;
 
 
-
 public class PropertyLoader {
+    private static Logger logger = Logger.getLogger(PropertyLoader.class);
 
 
     public static void loadEnvironmentValues(Properties properties) {
-        System.out.println("=================================");
         Enumeration keys = properties.propertyNames();
         while (keys.hasMoreElements()) {
             String key = (String) keys.nextElement();
@@ -38,10 +27,10 @@ public class PropertyLoader {
             System.out.println(key + ":" + oldValue);
             if (envValue != null) {
                 properties.setProperty(key, envValue);
-                System.out.println("Setting java property for key:" + key + " ,value:" + envValue + " ,oldValue:" + oldValue);
+                logger.debug("Setting java property for key:" + key + " ,value:" + envValue + " ,oldValue:" + oldValue);
             } else if (systemPropValue != null) {
                 properties.setProperty(key, systemPropValue);
-                System.out.println("Setting java property for key:" + key + " ,value:" + systemPropValue + " ,oldValue:"
+                logger.debug("Setting java property for key:" + key + " ,value:" + systemPropValue + " ,oldValue:"
                         + oldValue);
             }
         }
