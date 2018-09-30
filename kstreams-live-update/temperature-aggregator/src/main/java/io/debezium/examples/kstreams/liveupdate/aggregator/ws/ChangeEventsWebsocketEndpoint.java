7cc902b735e9c4916b57d5a964cdf7d9099eca62
Nils Hartmann <nils@nilshartmann.net>
2018-09-30 09:58:16 +0200

    [kstreams] Extracting KStreams builder creation to own class

--- kstreams-live-update/temperature-aggregator/src/main/java/io/debezium/examples/kstreams/liveupdate/aggregator/ws/ChangeEventsWebsocketEndpoint.java
+++ kstreams-live-update/temperature-aggregator/src/main/java/io/debezium/examples/kstreams/liveupdate/aggregator/ws/ChangeEventsWebsocketEndpoint.java
@@ -18,8 +18,6 @@
 package io.debezium.examples.kstreams.liveupdate.aggregator.ws;
 
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.Collections;
 import java.util.Properties;
 import java.util.Set;
@@ -35,28 +33,15 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
+import io.debezium.examples.kstreams.liveupdate.aggregator.TemperatureTableBuilder;
 import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.Consumed;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.kstream.Joined;
 import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KTable;
-import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.Produced;
-import org.apache.kafka.streams.kstream.Serialized;
-
-import io.debezium.examples.kstreams.liveupdate.aggregator.model.CountAndSum;
-import io.debezium.examples.kstreams.liveupdate.aggregator.model.Station;
-import io.debezium.examples.kstreams.liveupdate.aggregator.model.TemperatureMeasurement;
-import io.debezium.examples.kstreams.liveupdate.aggregator.serdes.CountAndSumSerde;
-import io.debezium.examples.kstreams.liveupdate.aggregator.serdes.LongKeySerde;
-import io.debezium.examples.kstreams.liveupdate.aggregator.serdes.StationSerde;
-import io.debezium.examples.kstreams.liveupdate.aggregator.serdes.TemperatureMeasurementSerde;
 
 @ServerEndpoint("/example")
 @ApplicationScoped
@@ -82,60 +67,20 @@ public class ChangeEventsWebsocketEndpoint {
 
         StreamsBuilder builder = new StreamsBuilder();
 
-        Serde<Long> longKeySerde = new LongKeySerde();
+      final KStream<String, String> avgTemperaturesByStation = TemperatureTableBuilder.avgTemperaturesByStation(builder)
+          .toStream()
+          .peek((k, v) -> sessions.forEach(s -> {
+            try {
+              s.getBasicRemote().sendText("{ \"station\" : \"" + k + "\", \"average-temperature\" : " + v + " }");
+            } catch (IOException e) {
+              throw new RuntimeException(e);
+            }
+          }));
 
-        Serde<TemperatureMeasurement> temperatureMeasurementsSerde = new TemperatureMeasurementSerde();
-        Serde<Station> stationSerde = new StationSerde();
-        Serde<CountAndSum> countAndSumSerde = new CountAndSumSerde();
-
-        KTable<Long, Station> stations = builder.table("dbserver1.inventory.stations", Consumed.with(longKeySerde, stationSerde));
-
-        KStream<String, String> maxTemperaturesByStation = builder.stream(
-                "dbserver1.inventory.temperature_measurements",
-                Consumed.with(longKeySerde, temperatureMeasurementsSerde)
-            )
-            .selectKey((k, v) -> v.stationId)
-            .join(
-                    stations,
-                    (value1, value2) -> {
-                        value1.stationName = value2.name;
-                        return value1;
-                    },
-                    Joined.with(Serdes.Long(), temperatureMeasurementsSerde, null)
-             )
-            .selectKey((k, v) -> v.stationName)
-            .groupByKey(Serialized.with(Serdes.String(), temperatureMeasurementsSerde))
-            .aggregate(
-                    () -> new CountAndSum(), /* initializer */
-                    (aggKey, newValue, aggValue) -> {
-                        aggValue.count++;
-                        aggValue.sum += newValue.value;
-                        return aggValue;
-                    },
-                    Materialized.with(Serdes.String(), countAndSumSerde)
-            )
-            .mapValues(v -> {
-                BigDecimal avg = BigDecimal.valueOf(v.sum / (double) v.count);
-                avg = avg.setScale(1, RoundingMode.HALF_UP);
-                return avg.doubleValue();
-            })
-            .mapValues(String::valueOf)
-            .toStream()
-            .peek((k, v) -> sessions.forEach(s -> {
-                try {
-                    s.getBasicRemote().sendText( "{ \"station\" : \"" + k + "\", \"average-temperature\" : " + v + " }" );
-                }
-                catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }));
-
-        maxTemperaturesByStation.to("average_temperatures_by_station", Produced.with(Serdes.String(), Serdes.String()));
-//        maxTemperaturesByStation.print(Printed.toSysOut());
+      avgTemperaturesByStation.to("average_temperatures_by_station", Produced.with(Serdes.String(), Serdes.String()));
 
         streams = new KafkaStreams(builder.build(), props);
         Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
-
         streams.start();
     }
 
