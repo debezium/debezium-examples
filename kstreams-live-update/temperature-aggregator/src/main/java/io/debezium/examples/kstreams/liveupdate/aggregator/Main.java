22acc346fd18a49718cd3290ba328af27d233e05
Gunnar Morling <gunnar.morling@googlemail.com>
2018-10-01 15:45:57 +0200

    [kstreams] Misc. clean-up

--- kstreams-live-update/temperature-aggregator/src/main/java/io/debezium/examples/kstreams/liveupdate/aggregator/Main.java
+++ kstreams-live-update/temperature-aggregator/src/main/java/io/debezium/examples/kstreams/liveupdate/aggregator/Main.java
@@ -1,4 +1,11 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
 package io.debezium.examples.kstreams.liveupdate.aggregator;
+
+import java.util.Properties;
 
 import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
@@ -10,46 +17,36 @@ import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Printed;
 import org.apache.kafka.streams.kstream.Produced;
 
-import java.util.Properties;
-
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    if (args.length != 1) {
-      System.err.println("usage: java -jar <package> "
-          + Main.class.getName() + " <bootstrap_servers>");
-      System.exit(-1);
+        if (args.length != 1) {
+            System.err.println("usage: java -jar <package> " + Main.class.getName() + " <bootstrap_servers>");
+            System.exit(-1);
+        }
+
+        final String bootstrapServers = args[0];
+
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "temperature-aggregator");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 10 * 1024);
+        props.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 1000);
+        props.put(CommonClientConfigs.METADATA_MAX_AGE_CONFIG, 500);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        StreamsBuilder builder = new StreamsBuilder();
+        final KTable<String, String> avgTemperaturesByStation = TemperatureTableBuilder
+                .avgTemperaturesByStation(builder);
+
+        avgTemperaturesByStation.toStream().to("average_temperatures_by_station",
+                Produced.with(Serdes.String(), Serdes.String()));
+        avgTemperaturesByStation.toStream().print(Printed.toSysOut());
+
+        final KafkaStreams streams = new KafkaStreams(builder.build(), props);
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+
+        streams.start();
     }
-
-    final String bootstrapServers = args[0];
-
-    Properties props = new Properties();
-    props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streaming-aggregates-ddd");
-    props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-    props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 10 * 1024);
-    props.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 1000);
-    props.put(CommonClientConfigs.METADATA_MAX_AGE_CONFIG, 500);
-    props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-
-    StreamsBuilder builder = new StreamsBuilder();
-    final KTable<String, String> avgTemperaturesByStation = TemperatureTableBuilder.avgTemperaturesByStation(builder);
-
-    avgTemperaturesByStation.toStream().to("average_temperatures_by_station", Produced.with(Serdes.String(), Serdes.String()));
-    avgTemperaturesByStation.toStream().print(Printed.toSysOut());
-
-    final KafkaStreams streams = new KafkaStreams(builder.build(), props);
-    Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
-
-    streams.start();
-  }
 }
-
-
-
-
-
-
-
-
-
