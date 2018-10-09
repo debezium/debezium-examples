14061d9bb4778a64d4d45f1497b8cec07d422121
Nils Hartmann <nils@nilshartmann.net>
2018-10-09 08:35:36 +0200

    Add "productId" filter to GraphQL subscription

--- graphql/aggregator/src/main/java/io/debezium/examples/graphql/SubscriptionResolver.java
+++ graphql/aggregator/src/main/java/io/debezium/examples/graphql/SubscriptionResolver.java
@@ -7,23 +7,32 @@ import org.reactivestreams.Publisher;
 import com.coxautodev.graphql.tools.GraphQLSubscriptionResolver;
 
 import io.debezium.examples.graphql.model.Order;
+import io.reactivex.Flowable;
 
 public class SubscriptionResolver implements GraphQLSubscriptionResolver {
 
     private static final Logger LOG = Logger.getLogger(SubscriptionResolver.class.getName());
 
-    private OrderPublisher publisher;
+    private OrderPublisher orderPublisher;
 
     public SubscriptionResolver(OrderPublisher publisher) {
-        this.publisher = publisher;
+        this.orderPublisher = publisher;
     }
 
-    public Publisher<Order> onNewOrder(final Integer withMinQuantity) {
-        LOG.info("New Subscription for orders " + (withMinQuantity != null ? "with at least " + withMinQuantity + " quantity" : ""));
-        if (withMinQuantity == null) {
-            return this.publisher.getPublisher();
+    public Publisher<Order> onNewOrder(final Integer withMinQuantity, final String withProductId ) {
+        LOG.info("New Subscription for orders " + (withMinQuantity != null ? "with at least " + withMinQuantity + " quantity" : "")
+                + (withMinQuantity != null ? " product id " + withProductId  : ""));
+        
+        Flowable<Order> publisher = this.orderPublisher.getPublisher();
+        
+        if (withMinQuantity != null) {
+            publisher = publisher.filter(o -> o.quantity >= withMinQuantity );
         }
-
-        return this.publisher.getPublisher().filter(o -> o.quantity >= withMinQuantity);
+        
+        if (withProductId != null) {
+            publisher = publisher.filter(o -> withProductId.equals(String.valueOf(o.productId)));
+        }
+        
+        return publisher;
     }
 }
