9be830fab615789ab4f430e7b74fefc2caa2a662
Gunnar Morling <gunnar.morling@googlemail.com>
2018-11-05 11:53:52 +0100

    [kstreams] Making aggregator a multi-stage Docker build;
    * One stage creates a minimal JDK with just the required modules
    * One stage builds the project and adds the dependencies
    * The final stage assembles all required files
    
    As a result, the resulting image is much smaller.

---
+++ kstreams/poc-ddd-aggregates/run-aggregator.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export PATH="/opt/poc-ddd-aggregates/jdk/bin:${PATH}"
+export JAVA_APP_DIR=/opt/poc-ddd-aggregates/lib
+export JAVA_MAIN_CLASS=io.debezium.examples.aggregation.StreamingAggregatesDDD
+
+exec  /opt/poc-ddd-aggregates/run-java.sh "$PARENT_TOPIC $CHILDREN_TOPIC $BOOTSTRAP_SERVERS"
