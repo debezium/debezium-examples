823743af9994f4c8237306cd40bc20725f256609
Gunnar Morling <gunnar.morling@googlemail.com>
2018-12-04 20:33:07 +0100

    DBZ-991 Using Infinispan as TX id cache (no CDI)

--- cache-invalidation/src/main/java/io/debezium/examples/cacheinvalidation/persistence/KnownTransactions.java
+++ cache-invalidation/src/main/java/io/debezium/examples/cacheinvalidation/persistence/KnownTransactions.java
@@ -1,9 +1,13 @@
 package io.debezium.examples.cacheinvalidation.persistence;
 
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.PreDestroy;
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
 
 import org.infinispan.Cache;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.manager.DefaultCacheManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,9 +20,26 @@ public class KnownTransactions {
 
     private static final Logger LOG = LoggerFactory.getLogger( KnownTransactions.class );
 
-    @Inject
-    @TransactionIdCache
-    private Cache<Long, Boolean> applicationTransactions;
+    private final DefaultCacheManager cacheManager;
+    private final Cache<Long, Boolean> applicationTransactions;
+
+    public KnownTransactions() {
+        cacheManager = new DefaultCacheManager();
+        cacheManager.defineConfiguration(
+                "tx-id-cache",
+                new ConfigurationBuilder()
+                    .expiration()
+                        .lifespan(60, TimeUnit.SECONDS)
+                    .build()
+                );
+
+        applicationTransactions = cacheManager.getCache("tx-id-cache");
+    }
+
+    @PreDestroy
+    public void stopCacheManager() {
+        cacheManager.stop();
+    }
 
     public void register(long txId) {
         LOG.info("Registering TX {} started by this application", txId);
