25c4be9fe7dfeb38fb4ec667c2c63df7cbd9e683
Gunnar Morling <gunnar.morling@googlemail.com>
2018-12-04 18:33:00 +0100

    DBZ-991 Using Infinispan as TX id cache

--- cache-invalidation/src/main/java/io/debezium/examples/cacheinvalidation/persistence/KnownTransactions.java
+++ cache-invalidation/src/main/java/io/debezium/examples/cacheinvalidation/persistence/KnownTransactions.java
@@ -1,14 +1,11 @@
 package io.debezium.examples.cacheinvalidation.persistence;
 
-import java.util.concurrent.TimeUnit;
-
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
+import org.infinispan.Cache;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.github.benmanes.caffeine.cache.Cache;
-import com.github.benmanes.caffeine.cache.Caffeine;
 
 /**
  * Keeps track of transactions initiated by this application, so we can tell
@@ -19,14 +16,9 @@ public class KnownTransactions {
 
     private static final Logger LOG = LoggerFactory.getLogger( KnownTransactions.class );
 
+    @Inject
+    @TransactionIdCache
     private Cache<Long, Boolean> applicationTransactions;
-
-    public KnownTransactions() {
-        applicationTransactions = Caffeine.newBuilder()
-                .maximumSize(10_000)
-                .expireAfterWrite(1, TimeUnit.MINUTES)
-                .build();
-    }
 
     public void register(long txId) {
         LOG.info("Registering TX {} started by this application", txId);
@@ -34,6 +26,6 @@ public class KnownTransactions {
     }
 
     public boolean isKnown(long txId) {
-        return Boolean.TRUE.equals(applicationTransactions.getIfPresent(txId));
+        return Boolean.TRUE.equals(applicationTransactions.get(txId));
     }
 }
---
+++ cache-invalidation/src/main/java/io/debezium/examples/cacheinvalidation/persistence/TransactionIdCache.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.examples.cacheinvalidation.persistence;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
+@Qualifier
+@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface TransactionIdCache {
+}
