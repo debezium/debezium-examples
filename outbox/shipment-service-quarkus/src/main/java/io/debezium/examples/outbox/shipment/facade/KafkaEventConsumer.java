c4b2f721798b0365d3e081fb29c76b7f90500794
Jiri Pechanec <jpechane@redhat.com>
2019-03-08 16:58:27 +0100

    [outbox] Adding native variant of shipment-service based on Quarkus;
    This requires a few changes due to certain APIs not yet supported by Quarkus for the time being:
    
    * Using SmallRye Messaging instead of Kafka consumer
    * Using Jackson for JSON processing
    * Using MariaDB
    * Adding readiness probe for MariaDB

---
+++ outbox/shipment-service-quarkus/src/main/java/io/debezium/examples/outbox/shipment/facade/KafkaEventConsumer.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.examples.outbox.shipment.facade;
+
+import java.io.IOException;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.CompletionStage;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.smallrye.reactive.messaging.kafka.KafkaMessage;
+
+@ApplicationScoped
+public class KafkaEventConsumer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaEventConsumer.class);
+
+    @Inject
+    OrderEventHandler orderEventHandler;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Incoming("orders")
+    public CompletionStage<Void> onMessage(KafkaMessage<String, String> message) throws IOException {
+        try {
+            LOG.debug("Kafka message with key = {} arrived", message.getKey());
+            final Optional<String> eventId = message.getHeaders().getOneAsString("eventId");
+            if (eventId.isPresent()) {
+                orderEventHandler.onOrderEvent(
+                        UUID.fromString(eventId.get()),
+                        message.getKey(),
+                        objectMapper.readTree(message.getPayload())
+                );
+            }
+            else {
+                LOG.warn("Skipping Kafka message with key = {}, eventId header was missing");
+            }
+        }
+        catch (Throwable t) {
+            LOG.error("Error in message processing", t);
+        }
+        return message.ack();
+    }
+}
---
+++ outbox/shipment-service-quarkus/src/main/java/io/debezium/examples/outbox/shipment/service/ShipmentService.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.examples.outbox.shipment.service;
+
+import java.time.LocalDateTime;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import javax.transaction.Transactional.TxType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import io.debezium.examples.outbox.shipment.model.Shipment;
+
+@ApplicationScoped
+public class ShipmentService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ShipmentService.class);
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Transactional(value=TxType.MANDATORY)
+    public void orderCreated(JsonNode event) {
+        LOGGER.info("Processing 'OrderCreated' event: {}", event);
+
+        final long orderId = event.get("id").asLong();
+        final long customerId = event.get("customerId").asLong();
+        final LocalDateTime orderDate = LocalDateTime.parse(event.get("orderDate").asText());
+
+        entityManager.persist(new Shipment(customerId, orderId, orderDate));
+    }
+
+    @Transactional(value=TxType.MANDATORY)
+    public void orderLineUpdated(JsonNode event) {
+        LOGGER.info("Processing 'OrderLineUpdated' event: {}", event);
+    }
+}
