5bdc1fedc54a29c8981973e69640c04a874dced1
Dave Cramer <davecramer@gmail.com>
2019-12-19 06:54:51 -0500

    make example work with pg

--- kstreams-live-update/example-db/schema-update.sql
+++ kstreams-live-update/example-db/schema-update.sql
@@ -1,33 +1,32 @@
-# Switch to this database
-USE inventory;
+
+set search_path='inventory';
 
 DROP TABLE IF EXISTS orders;
 
 CREATE TABLE categories (
-  id BIGINT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(255) NOT NULL,
-  average_price BIGINT,
-  PRIMARY KEY (id)
-) AUTO_INCREMENT = 100001;
+  id serial primary key,
+  name text NOT NULL,
+  average_price int8
+);
 
-INSERT INTO categories VALUES (default, 'Toys', 4500);
-INSERT INTO categories VALUES (default, 'Books', 2200);
-INSERT INTO categories VALUES (default, 'Computers', 6700);
-INSERT INTO categories VALUES (default, 'Tools', 4800);
-INSERT INTO categories VALUES (default, 'Plants', 1900);
-INSERT INTO categories VALUES (default, 'Food', 500);
-INSERT INTO categories VALUES (default, 'Furniture', 2700);
-INSERT INTO categories VALUES (default, 'Cloth', 3700);
+INSERT INTO categories (name, average_price) VALUES ('Toys', 4500);
+INSERT INTO categories (name, average_price) VALUES ('Books', 2200);
+INSERT INTO categories (name, average_price) VALUES ('Computers', 6700);
+INSERT INTO categories (name, average_price) VALUES ('Tools', 4800);
+INSERT INTO categories (name, average_price) VALUES ('Plants', 1900);
+INSERT INTO categories (name, average_price) VALUES ('Food', 500);
+INSERT INTO categories (name, average_price) VALUES ('Furniture', 2700);
+INSERT INTO categories (name, average_price) VALUES ('Cloth', 3700);
 
 CREATE TABLE orders (
-  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id serial PRIMARY KEY,
   ts TIMESTAMP NOT NULL,
-  purchaser_id INTEGER NOT NULL,
-  product_id INTEGER NOT NULL,
-  category_id BIGINT NOT NULL,
-  quantity INTEGER NOT NULL,
-  sales_price BIGINT NOT NULL
-) AUTO_INCREMENT = 100001;
+  purchaser_id int4 NOT NULL,
+  product_id int4 NOT NULL,
+  category_id int8 NOT NULL,
+  quantity int4 NOT NULL,
+  sales_price int8  NOT NULL
+);
 
 ALTER TABLE orders ADD CONSTRAINT fk_orders_product_id FOREIGN KEY (product_id) REFERENCES inventory.products(id);
 ALTER TABLE orders ADD CONSTRAINT fk_orders_purchaser_id FOREIGN KEY (purchaser_id) REFERENCES inventory.customers(id);
