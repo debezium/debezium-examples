42909d7d325143f6d621108b82c34716df63e138
Gunnar Morling <gunnar.morling@googlemail.com>
2020-01-07 18:05:36 +0100

    DBZ-1478 Bringing back message log classes

---
+++ outbox/shipment-service/src/main/java/io/debezium/examples/outbox/shipment/messagelog/ConsumedMessage.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.examples.outbox.shipment.messagelog;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class ConsumedMessage {
+
+    @Id
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID eventId;
+
+    private Instant timeOfReceiving;
+
+    ConsumedMessage() {
+    }
+
+    public ConsumedMessage(UUID eventId, Instant timeOfReceiving) {
+        this.eventId = eventId;
+        this.timeOfReceiving = timeOfReceiving;
+    }
+
+    public UUID getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(UUID eventId) {
+        this.eventId = eventId;
+    }
+
+    public Instant getTimeOfReceiving() {
+        return timeOfReceiving;
+    }
+
+    public void setTimeOfReceiving(Instant timeOfReceiving) {
+        this.timeOfReceiving = timeOfReceiving;
+    }
+}
