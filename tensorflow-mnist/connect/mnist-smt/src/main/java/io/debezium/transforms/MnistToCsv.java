7b66b6a9f1462fe86821e129e5fe16672cbc7153
Vojtech Juranek <vjuranek@redhat.com>
2023-04-23 23:52:05 +0200

    [tensorflow] Fix more typos

--- tensorflow-mnist/connect/mnist-smt/src/main/java/io/debezium/transforms/MnistToCsv.java
+++ tensorflow-mnist/connect/mnist-smt/src/main/java/io/debezium/transforms/MnistToCsv.java
@@ -38,7 +38,7 @@ public class MnistToCsv<R extends ConnectRecord<R>> implements Transformation<R>
     @Override
     public void configure(Map<String, ?> map) {
         labelFieldName = map.containsKey(LABEL_CONFIG) ? (String) map.get(LABEL_CONFIG) : LABEL_COLUMN_DEFAULT;
-        pixlesFieldName = map.containsKey(PIXEL_CONFIG) ? (String) map.get(PIXEL_CONFIG) : PIZELS_COLUMN_DEFAULT;
+        pixlesFieldName = map.containsKey(PIXEL_CONFIG) ? (String) map.get(PIXEL_CONFIG) : PIXELS_COLUMN_DEFAULT;
     }
 
     @Override
