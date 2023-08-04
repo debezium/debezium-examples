b22fdf5872e5b8cf11866fc2009099d4ec23ffb1
Chris Cranford <chris@hibernate.org>
2023-08-04 18:46:44 -0400

    [Distributed Caching] Switch to Debezium 2.3.2.Final; update deps
    * Update to Debezium 2.3.2.Final
    * Update to Quarkus 3.2.3.Final
    * Update to maven-surefire-plugin 3.0.0-M6

--- distributed-caching/order-service/src/main/java/io/debezium/examples/caching/order/rest/OrderResource.java
+++ distributed-caching/order-service/src/main/java/io/debezium/examples/caching/order/rest/OrderResource.java
@@ -5,15 +5,15 @@
  */
 package io.debezium.examples.caching.order.rest;
 
-import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.NotFoundException;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
