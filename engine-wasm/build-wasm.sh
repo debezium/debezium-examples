9268d2493a435bb53583eb87d8b3970dccef97c2
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2024-11-12 12:12:11 +0000

    Improve TinyGo integration

--- engine-wasm/build-wasm.sh
+++ engine-wasm/build-wasm.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+set -euo pipefail
 
-cd "$PWD/src/main/resources/go"
-tinygo build -target=wasi -o ../compiled/cdc.wasm cdc.go
-cd "$OLDPWD"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+docker run --rm \
+    -v ${SCRIPT_DIR}/src/main/resources:/src \
+    -w /src tinygo/tinygo:0.34.0 bash \
+    -c "tinygo build -ldflags='-extldflags --import-memory' --no-debug -target=wasm-unknown -o /tmp/cdc.wasm go/cdc.go && cat /tmp/cdc.wasm" > \
+    ${SCRIPT_DIR}/src/main/resources/compiled/cdc.wasm
--- engine-wasm/src/main/resources/go/cdc.go
+++ engine-wasm/src/main/resources/go/cdc.go
@@ -6,6 +6,35 @@ import (
     "unsafe"
 )
 
+// alloc/free implementation from:
+// https://github.com/tinygo-org/tinygo/blob/2a76ceb7dd5ea5a834ec470b724882564d9681b3/src/runtime/arch_tinygowasm_malloc.go#L7
+var allocs = make(map[uintptr][]byte)
+
+//go:wasm-module cdc
+//export malloc
+func libc_malloc(size uintptr) unsafe.Pointer {
+	if size == 0 {
+		return nil
+	}
+	buf := make([]byte, size)
+	ptr := unsafe.Pointer(&buf[0])
+	allocs[uintptr(ptr)] = buf
+	return ptr
+}
+
+//go:wasm-module cdc
+//export free
+func libc_free(ptr unsafe.Pointer) {
+	if ptr == nil {
+		return
+	}
+	if _, ok := allocs[uintptr(ptr)]; ok {
+		delete(allocs, uintptr(ptr))
+	} else {
+		panic("free: invalid pointer")
+	}
+}
+
 func readBufferFromMemory(bufferPosition *uint32, length uint32) []byte {
     subjectBuffer := make([]byte, length)
     pointer := uintptr(unsafe.Pointer(bufferPosition))
