19c592790f89564a13f4f600802f97b816a031d7
Vojtech Juranek <vjuranek@redhat.com>
2025-11-03 22:27:00 +0100

    [cqrs] Support multiple polls in the UI

--- cqrs/poll-results-service/src/main/java/io/debezium/examples/cqrs/ResultsResource.java
+++ cqrs/poll-results-service/src/main/java/io/debezium/examples/cqrs/ResultsResource.java
@@ -3,6 +3,7 @@ package io.debezium.examples.cqrs;
 import java.util.List;
 
 import io.debezium.examples.cqrs.entity.OptionVotesEntity;
+import io.debezium.examples.cqrs.entity.PollEntity;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Query;
@@ -25,17 +26,44 @@ import org.slf4j.LoggerFactory;
 public class ResultsResource {
     private static final Logger LOGGER = LoggerFactory.getLogger(ResultsResource.class);
 
-    @ConfigProperty(name="results.query")
-    private String resultQuery;
+    private static final String POLL_QUERY = "SELECT id, question FROM pollentity";
+    private static final String VOTE_QUERY = """
+            SELECT optionentity.polloption AS \"option\", count(voteentity.votedoption) AS votes  
+            FROM voteentity JOIN optionentity ON voteentity.votedoption = optionentity.id 
+            WHERE voteentity.pollid = :pollId 
+            GROUP BY optionentity.polloption;
+            """;
 
     @Inject
     private EntityManager em;
 
     @GET
+    public List<PollEntity> getPolls() {
+        try {
+            Query q = em.createNativeQuery(POLL_QUERY, PollEntity.class);
+            List<PollEntity> polls = q.getResultList();
+            for (PollEntity poll : polls) {
+                LOGGER.info("poll id: {}, question: {}", poll.id, poll.question);
+            }
+            return polls;
+        }
+        catch (Exception e) {
+            if (e.getMessage().contains("table does not exist [table=pollentity]")) {
+                LOGGER.warn("Table 'pollentity' does not exist, have you already deployed source and sink connectors?");
+            }
+            else {
+                throw e;
+            }
+        }
+        return null;
+    }
+
+    @GET
     @Path("{id}")
     public List<OptionVotesEntity> getResult(Long id) {
         try {
-            Query q = em.createNativeQuery(resultQuery, OptionVotesEntity.class);
+            Query q = em.createNativeQuery(VOTE_QUERY, OptionVotesEntity.class);
+            q.setParameter("pollId", id);
             List<OptionVotesEntity> res = q.getResultList();
             for (OptionVotesEntity o : res) {
                 LOGGER.info("result: {}, votes: {}", o.option, o.votes);
--- cqrs/poll-results-service/src/main/webui/src/App.js
+++ cqrs/poll-results-service/src/main/webui/src/App.js
@@ -1,13 +1,59 @@
 import './App.css';
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+    BrowserRouter as Router,
+    Link,
+    Route,
+    Routes
+} from "react-router";
 import VotesChart from "./VotesChart";
 
 function App() {
-  return (
-    <div className="App">
-	<VotesChart />
-    </div>
-  );
+    const API_URL = "http://127.0.0.1:8081/results/";
+
+    const [polls, setData] = useState([]);
+    const fetchPolls = async () => {
+        try {
+            const response = await fetch(API_URL);
+            if (!response.ok) throw new Error("Failed to fetch data");
+            const polls = await response.json();
+            setData(polls);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
+
+    // Polling
+    useEffect(() => {
+        fetchPolls(); // Initial fetch
+        const interval = setInterval(fetchPolls, 5000); // Poll every 5s
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <div className="App">
+            <Router>
+                <div>
+                    <h1>Polls</h1>
+                    <nav>
+                        <ul>
+                            {polls.map(({id, question}) => (
+                                <li key={id}>
+                                    <Link to={`view/${id}`}>{question}</Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </nav>
+                    <Routes>
+                        <Route
+                            path="/view/:id"
+                            element={<VotesChart/>}
+                        />
+                    </Routes>
+                </div>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
