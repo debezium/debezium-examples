4740a608238ff3d66637ac920200ec13afdf3971
Ian Sawala <iansawalasaidi@gmail.com>
2026-03-19 15:27:26 +0300

    DBZ-313:Demo: Keep *all* change events in a downstream database [DBZ-2101] + Modyfying README.md and connector configuration for .add.fields.prefix
    Signed-off-by: Ian Sawala <iansawalasaidi@gmail.com>

--- auditlog/connector-config/scripts/init.sql
+++ auditlog/connector-config/scripts/init.sql
@@ -26,31 +26,3 @@ CREATE TABLE dbserver1_inventory_vegetable_enriched (
 PRIMARY KEY (__connect_offset, __connect_partition, __connect_topic)
 );
 
-
--- This version is used with the Camel JDBC Sink Connector
-CREATE TABLE enriched_vegetables (
-    -- Business Data
-    id INTEGER,
-    name VARCHAR(255) NULL,
-    description TEXT NULL,
-    
-    -- Audit Metadata
-    tx_id VARCHAR(255),
-    user_name VARCHAR(255) NULL,
-    use_case VARCHAR(255) NULL,
-    client_date BIGINT NULL,
-    
-    -- Change Metadata (for uniqueness)
-    op VARCHAR(1),
-    lsn BIGINT,
-    ts_ms BIGINT,
-
-    -- Kafka Metadata to prevent squashing of events
-    kafka_partion INTEGER,
-    kafka_offset BIGINT,
-    kafka_topic VARCHAR(255),
-    
-    -- To ensure that all events are kept we will use kafka metadata to 
-    -- 
-PRIMARY KEY (kafka_topic, kafka_partition, kafka_offset)
-);
