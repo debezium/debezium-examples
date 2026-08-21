52ec867c75383287bc282b26c09723dcf03caa51
Jiri Pechanec <jiri.pechanec@centrum.cz>
2026-08-21 14:29:28 +0200

    [distributed-caching] Align with Debezium 3.6.1.Final
    Signed-off-by: Jiri Pechanec <jiri.pechanec@centrum.cz>

--- distributed-caching/order-service/src/main/java/io/debezium/examples/caching/order/rest/util/OptimisticLockMapper.java
+++ distributed-caching/order-service/src/main/java/io/debezium/examples/caching/order/rest/util/OptimisticLockMapper.java
@@ -5,10 +5,10 @@
  */
 package io.debezium.examples.caching.order.rest.util;
 
-import javax.persistence.OptimisticLockException;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.ext.ExceptionMapper;
-import javax.ws.rs.ext.Provider;
+import jakarta.persistence.OptimisticLockException;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
 
 import io.debezium.examples.caching.commons.EntityNotFoundException;
 
