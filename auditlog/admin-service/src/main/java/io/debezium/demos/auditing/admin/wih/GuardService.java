a4591cfd0536779895a0654b5f0655ae4c30099f
Jiri Pechanec <jiri.pechanec@centrum.cz>
2026-08-24 10:31:12 +0200

    [auditlog] Add missing class
    Signed-off-by: Jiri Pechanec <jiri.pechanec@centrum.cz>

---
+++ auditlog/admin-service/src/main/java/io/debezium/demos/auditing/admin/wih/GuardService.java
@@ -0,0 +1,10 @@
+package io.debezium.demos.auditing.admin.wih;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+/**
+ * Guard service placeholder referenced by the BPMN process definitions.
+ */
+@ApplicationScoped
+public class GuardService {
+}
